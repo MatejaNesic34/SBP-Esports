7292063c303ac91cc060b0dfaef81cbddfcebbad
Mateja <mateja.nesic@elfak.rs>
2026-08-07 11:32:29 +0200

    Sample podaci su dodati za skoro sve tabele

--- II-Projekat/ESPORT/ESPORT/Form1.Designer.cs
+++ II-Projekat/ESPORT/ESPORT/Form1.Designer.cs
@@ -38,69 +38,76 @@
             // 
             // osobebtn
             // 
-            osobebtn.Location = new Point(157, 105);
+            osobebtn.Location = new Point(74, 51);
+            osobebtn.Margin = new Padding(1, 1, 1, 1);
             osobebtn.Name = "osobebtn";
-            osobebtn.Size = new Size(188, 58);
+            osobebtn.Size = new Size(115, 28);
             osobebtn.TabIndex = 0;
             osobebtn.Text = "Osobe";
             osobebtn.UseVisualStyleBackColor = true;
             // 
             // timbtn
             // 
-            timbtn.Location = new Point(157, 211);
+            timbtn.Location = new Point(74, 103);
+            timbtn.Margin = new Padding(1, 1, 1, 1);
             timbtn.Name = "timbtn";
-            timbtn.Size = new Size(188, 58);
+            timbtn.Size = new Size(115, 28);
             timbtn.TabIndex = 1;
             timbtn.Text = "Tim";
             timbtn.UseVisualStyleBackColor = true;
             // 
             // takmicenjebtn
             // 
-            takmicenjebtn.Location = new Point(157, 320);
+            takmicenjebtn.Location = new Point(74, 156);
+            takmicenjebtn.Margin = new Padding(1, 1, 1, 1);
             takmicenjebtn.Name = "takmicenjebtn";
-            takmicenjebtn.Size = new Size(188, 58);
+            takmicenjebtn.Size = new Size(115, 28);
             takmicenjebtn.TabIndex = 2;
             takmicenjebtn.Text = "Takmicenje";
             takmicenjebtn.UseVisualStyleBackColor = true;
             // 
             // Mecbtn
             // 
-            Mecbtn.Location = new Point(157, 423);
+            Mecbtn.Location = new Point(74, 206);
+            Mecbtn.Margin = new Padding(1, 1, 1, 1);
             Mecbtn.Name = "Mecbtn";
-            Mecbtn.Size = new Size(188, 58);
+            Mecbtn.Size = new Size(115, 28);
             Mecbtn.TabIndex = 3;
             Mecbtn.Text = "Mec";
             Mecbtn.UseVisualStyleBackColor = true;
             // 
             // sponzorbtn
             // 
-            sponzorbtn.Location = new Point(159, 525);
+            sponzorbtn.Location = new Point(75, 256);
+            sponzorbtn.Margin = new Padding(1, 1, 1, 1);
             sponzorbtn.Name = "sponzorbtn";
-            sponzorbtn.Size = new Size(188, 58);
+            sponzorbtn.Size = new Size(114, 28);
             sponzorbtn.TabIndex = 4;
             sponzorbtn.Text = "Sponzor";
             sponzorbtn.UseVisualStyleBackColor = true;
             // 
             // igrebtn
             // 
-            igrebtn.Location = new Point(162, 621);
+            igrebtn.Location = new Point(76, 303);
+            igrebtn.Margin = new Padding(1, 1, 1, 1);
             igrebtn.Name = "igrebtn";
-            igrebtn.Size = new Size(188, 58);
+            igrebtn.Size = new Size(113, 28);
             igrebtn.TabIndex = 5;
             igrebtn.Text = "Igre";
             igrebtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1176, 770);
+            ClientSize = new Size(553, 376);
             Controls.Add(igrebtn);
             Controls.Add(sponzorbtn);
             Controls.Add(Mecbtn);
             Controls.Add(takmicenjebtn);
             Controls.Add(timbtn);
             Controls.Add(osobebtn);
+            Margin = new Padding(1, 1, 1, 1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
